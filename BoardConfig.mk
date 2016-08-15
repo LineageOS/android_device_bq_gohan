0681c3c4aa0e0c87f32a6ebb0bdcf297b3950f15
LuK1337 <priv.luk@gmail.com>
2016-08-15 15:41:06 +0200

    gohan: init: Add 3GB RAM / 32GB storage support
    * Set dalvik properties (increase heap size) dynamically based on amount of RAM..
    * Remove duplicate HWUI and dalvik properties
    * Update device description
    
    Change-Id: Iced20b769bc22660f27a0f34034119dff1f0ec87

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,7 +136,9 @@ USE_DEVICE_SPECIFIC_LOC_API := true
 TARGET_NO_RPC := true
 
 # Init
+TARGET_INIT_VENDOR_LIB := libinit_gohan
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+TARGET_RECOVERY_DEVICE_MODULES := libinit_gohan
 
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
