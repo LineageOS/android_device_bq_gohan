343aceb63a1e55d7d04d0fea31ff4887654b1974
Christian Morlok <christianmorlok@gmail.com>
2016-11-03 06:33:51 +0100

    gohan: Update fingerprint from 1.5.0 release
    Change-Id: I6f672d3fa6001371989dc9bc86ff5ff84612854e

--- cm.mk
+++ cm.mk
@@ -41,6 +41,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-bq
 # Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=bq/Aquaris_X5_Plus/Aquaris_X5_Plus:6.0.1/MMB29M/1472045538:user/release-keys \
-    PRIVATE_BUILD_DESC="gohan-user 6.0.1 MMB29M 1.3.0_20160824-1532 release-keys"
+    BUILD_FINGERPRINT=bq/Aquaris_X5_Plus/Aquaris_X5_Plus:6.0.1/MMB29M/1477468664:user/release-keys \
+    PRIVATE_BUILD_DESC="gohan-user 6.0.1 MMB29M 1.5.0_20161026-0957 release-keys"
 endif
