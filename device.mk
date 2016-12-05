1309015961a0e0cfae67eb6d06202ebcf703066a
darknightmare <rubentkd98@gmail.com>
2016-12-05 13:11:05 +0100

    gohan: Enable fingerprint support
     # Conflicts:
     #	rootdir/etc/init.target.rc
     #	sepolicy/file.te
     #	sepolicy/file_contexts
     #	sepolicy/fingerprintd.te
     #	system.prop
    
    Change-Id: Ieb4eba81934b048aba7370af1727696b1b5e4a34

--- device.mk
+++ device.mk
@@ -44,6 +44,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml\
     frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml\
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
@@ -109,6 +110,10 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8952 \
     liboverlay \
     memtrack.msm8952
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd
 
 # FM
 PRODUCT_PACKAGES += \
