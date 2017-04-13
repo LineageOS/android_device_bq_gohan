fa190cdc0dd4659ee8f793494dc3b2e17d8b4861
Kra1o5 <kra1o5x@gmail.com>
2017-04-13 23:47:25 +0200

    gohan: Update wlan config
    Change-Id: Ia9f28f7f1c408b7c53a64710fbfbcefbee0c1440

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,19 +178,16 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
 # Wifi
-WPA_SUPPLICANT_VERSION      := VER_0_8_X
-BOARD_HAS_QCOM_WLAN         := true
-BOARD_HAS_QCOM_WLAN_SDK     := true
-BOARD_WLAN_DEVICE           := qcwcn
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HAS_QCOM_WLAN_SDK := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER        := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-TARGET_USES_QCOM_WCNSS_QMI  := true
-WIFI_DRIVER_FW_PATH_AP      := "ap"
-WIFI_DRIVER_FW_PATH_STA     := "sta"
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME     := "wlan"
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/bq/gohan/BoardConfigVendor.mk
